9b072ce42fece3b130debe1da70b15eaf7a3bd87
elie ntumba <elientumba@live.fr>
2018-02-19 15:08:09 +0800

    reset button

--- color.js
+++ color.js
@@ -1,17 +1,13 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-];
 
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
-
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+
 
 colorDisplay.textContent = pickedColor;
 
@@ -28,6 +24,7 @@ for(var i = 0; i < squares.length; i++){
         if(clickedColor === pickedColor) {
             messageDisplay.textContent = "Correct";
             changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor;
         }
 
         else {
@@ -57,4 +54,41 @@ function pickColor() {
    var random =  Math.floor((Math.random() * colors.length));
    return colors[random];
 }
+
+
+/**
+ * generate random Array
+ */
+function generateRandomColors(num) {
+    var array = [];
+
+    for(var c = 0 ; c < num ; c++){
+        array.push(randomColor());
+    }
+    return array;
+}
+
+
+/**
+ * generate a single color
+ */
+function randomColor() {
+     var red = Math.floor(Math.random() * 256);
+     var green = Math.floor(Math.random() * 256);
+     var blues = Math.floor(Math.random() * 256);
+
+     return "rgb(" + red + ", " + green + ", " + blues + ")";
+}
+
+
+resetButton.addEventListener("click" , function () {
+
+    colors = generateRandomColors(6);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+
+    for(var c = 0 ; c < squares ;c++){
+        squares[c].style.background = colors[c];
+    }
+});
 
